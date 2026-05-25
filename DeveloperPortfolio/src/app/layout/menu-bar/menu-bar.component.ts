7027ae9fc93694a51b606e72224f9d460236c35f
Shehan-v <vindikas@ccl.lk>
2026-05-25 20:09:40 +0530

    Development - Create Menu List

--- DeveloperPortfolio/src/app/layout/menu-bar/menu-bar.component.ts
+++ DeveloperPortfolio/src/app/layout/menu-bar/menu-bar.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { OutletContext } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
 export class MenuBarComponent {
+  @Output() tabName = new EventEmitter<any>();
+
+  onTabChange(event: any) {
+    const selectedTab = event.tab.textLabel;
+    this.tabName.emit(selectedTab);
+    console.log('Selected Tab:', selectedTab);
+  }
 
 }
