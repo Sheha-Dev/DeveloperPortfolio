a1de3df9dd7f8c3c399b7a4289bc915d29bbfb26
Shehan-v <vindikas@ccl.lk>
2026-05-25 20:08:45 +0530

    Development - Call Full Component

--- DeveloperPortfolio/src/app/app.component.ts
+++ DeveloperPortfolio/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FullComponent } from "./layout/full/full.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FullComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
