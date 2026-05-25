5040531b2a8b8ae0d0433568072c5047bf9f432f
Shehan-v <vindikas@ccl.lk>
2026-05-25 20:11:18 +0530

    Development - Main Component Design Implemented

--- DeveloperPortfolio/src/app/layout/full/full.component.ts
+++ DeveloperPortfolio/src/app/layout/full/full.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { MatCardActions, MatCardContent, MatCardModule, MatCardTitle } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MenuBarComponent } from '../menu-bar/menu-bar.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-full',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatCardContent,
+    MenuBarComponent,
+    MatTabsModule,
+    CommonModule
+  ],
   templateUrl: './full.component.html',
   styleUrl: './full.component.scss'
 })
-export class FullComponent {
+export class FullComponent implements OnInit, OnDestroy ,OnChanges{
 
+  selectedTab: string = 'Home';
+
+
+  ngOnInit() {
+    console.log('FullComponent initialized');
+  }
+
+  ngOnChanges() {
+    console.log('FullComponent changes detected');
+  } 
+
+  ngOnDestroy() {
+    console.log('FullComponent destroyed');
+  }
 }
