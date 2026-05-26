9a125a8e9d4a95a424e95dd1b231be6bf20be31f
Shehan-v <vindikas@ccl.lk>
2026-05-26 10:02:03 +0530

    Development - Add Hamburger Menu for Small Screen

--- DeveloperPortfolio/src/app/layout/menu-bar/menu-bar.component.ts
+++ DeveloperPortfolio/src/app/layout/menu-bar/menu-bar.component.ts
@@ -1,20 +1,45 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenu, MatMenuModule } from '@angular/material/menu';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
 import { OutletContext } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [MatTabsModule],
+  imports: [
+    MatTabsModule,
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule
+  ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss'
 })
 export class MenuBarComponent {
   @Output() tabName = new EventEmitter<any>();
 
-  onTabChange(event: any) {
-    const selectedTab = event.tab.textLabel;
-    this.tabName.emit(selectedTab);
-    console.log('Selected Tab:', selectedTab);
+  isMobile = window.innerWidth <= 768;
+
+  tabs = [
+    { label: 'Home' },
+    { label: 'Education' },
+    { label: 'Projects' },
+    { label: 'Experience' },
+    { label: 'Contact' }
+  ];
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth <= 768;
   }
 
+  onTabChange(event: MatTabChangeEvent) {
+    console.log(event.index);
+    // your existing navigation logic
+  }
+
+  selectMenuTab(index: number) {
+    this.onTabChange({ index } as MatTabChangeEvent);
+  }
 }
