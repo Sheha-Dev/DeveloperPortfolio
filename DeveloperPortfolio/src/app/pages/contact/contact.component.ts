fd6bd79473290a60a9534ba8ee4c6c802dd45e37
Shehan-v <vindikas@ccl.lk>
2026-05-26 18:04:13 +0530

    Development - Made Responsive Contact Page

--- DeveloperPortfolio/src/app/pages/contact/contact.component.ts
+++ DeveloperPortfolio/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      sectionLabel: ["LET'S CONNECT"],
+      heading: ["I'd love to"],
+      headingHighlight: ['hear from you'],
+      description: [
+        `Whether you have a project idea, job opportunity, collaboration,
+        or just want to say hello — feel free to reach out.
+        I'm always open to meaningful conversations.`
+      ],
+
+      email: ['shehanvindika429@gmail.com'],
+      phone: ['+94 75 7500597'],
+      location: ['Sri Lanka'],
+
+      githubUrl: ['https://github.com/Sheha-Dev'],
+      linkedinUrl: ['https://www.linkedin.com/in/vindika-shehan-504029389/'],
+
+      name: ['', Validators.required],
+      senderEmail: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  get f() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.contactForm.value);
+  }
 }
