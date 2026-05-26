1fec8c1fc3889131f0982647f47fd253958ac006
Shehan-v <vindikas@ccl.lk>
2026-05-26 18:04:51 +0530

    Development - Made Responsive About Page

--- DeveloperPortfolio/src/app/pages/about/about.component.ts
+++ DeveloperPortfolio/src/app/pages/about/about.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  IconBrandGithub,
+  IconBrandLinkedin
+} from '@tabler/icons-angular';
 
 @Component({
   selector: 'app-about',
+  standalone: true,
   imports: [],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  githubIcon = IconBrandGithub;
+  linkedinIcon = IconBrandLinkedin;
 }
